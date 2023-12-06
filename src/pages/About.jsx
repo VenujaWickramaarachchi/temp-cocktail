d2c3d92fb2265c39d71364b668d75e4947163fd6
VenujaWickramaarachchi <venuja071@gmail.com>
2023-12-06 15:16:53 +0530

    first commit

---
+++ src/pages/About.jsx
@@ -0,0 +1,16 @@
+import Wrapper from '../assets/wrappers/AboutPage'
+
+const About = () => {
+  return (
+    <Wrapper>
+      <h3>About Us</h3>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum
+        excepturi voluptate necessitatibus quis minus doloribus repudiandae,
+        tempora eligendi aut molestias explicabo vel repellat voluptatum dolorum
+        velit distinctio dicta praesentium ad?
+      </p>
+    </Wrapper>
+  )
+}
+export default About
